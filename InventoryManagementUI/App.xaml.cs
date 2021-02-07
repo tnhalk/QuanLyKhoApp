aef22d7551c4f8be675c7406384e79f9be3dcae6
tnhalk <tnhalk@thiendiep.com>
2021-02-07 15:27:36 +0700

    Create GIT

---
+++ InventoryManagementUI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace InventoryManagementUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
